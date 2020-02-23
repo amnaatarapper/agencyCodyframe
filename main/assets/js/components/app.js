0b59b2033dffc48eabcd01ba75a00a77cb554f3d
Oussama <oussama.aitbaziz@gmail.com>
2020-02-23 10:08:48 +0100

    hero odne

--- main/assets/js/components/app.js
+++ main/assets/js/components/app.js
@@ -1,1 +1,16 @@
-console.log('Linked');
+// Style Navbar on scroll event
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('scroll', window.scrollY > 0);
+});
+
+
+// Parallax Effect on Hero BG on mouse mouvement
+window.addEventListener('mousemove', (e) => {
+    var hero = document.querySelector('#hero');
+
+    var mouseX = e.clientX / 100;
+    var mouseY = e.clientY / 100;
+
+    hero.style.backgroundPosition = mouseX + "px " + mouseY + "px";
+});
--- main/assets/js/scripts.js
+++ main/assets/js/scripts.js
@@ -315,4 +315,19 @@ Math.easeInOutQuad = function (t, b, c, d) {
 		return (element.offsetWidth || element.offsetHeight || element.getClientRects().length);
 	};
 }());
-console.log('Linked');
+// Style Navbar on scroll event
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('scroll', window.scrollY > 0);
+});
+
+
+// Parallax Effect on Hero BG on mouse mouvement
+window.addEventListener('mousemove', (e) => {
+    var hero = document.querySelector('#hero');
+
+    var mouseX = e.clientX / 100;
+    var mouseY = e.clientY / 100;
+
+    hero.style.backgroundPosition = mouseX + "px " + mouseY + "px";
+});
